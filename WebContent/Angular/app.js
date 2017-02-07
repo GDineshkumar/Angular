fdede02bf0f29e0544a8b7446c0a2bfe368df5e0
dines <dines@LAPTOP-KBLAK8OJ>
2017-02-06 20:36:05 -0500

    initial commit of the anugular project

---
+++ WebContent/Angular/app.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+
+var app = angular.module('angularModule',[]);
+
+app.controller('InboxCtrl', function ($scope) {
+	   // initialize the title property to an array for the view to use
+	   $scope.title = "Angular app";
+	});
